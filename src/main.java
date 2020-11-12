3b0a44b0c13c9c1ed43886638e3cd3b961b8f14d
семен семен <simon96pikalov@gmail.com>
2020-11-12 15:22:57 +0200

    first commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        try {
+            Desktop.getDesktop().browse(new URI("https://github.com/simon-pikalov/Ariel_OOP_2020/tree/master/Classes/week_03/ta3/simon_group"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
